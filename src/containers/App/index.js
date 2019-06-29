eb2aa7c000af41680b05ca9064db158c018ed529
Adam Jaffe Back <adb33@email.arizona.edu>
2019-06-29 15:47:52 -0700

    (feat) Use StoryList in App to display stories.

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -1,15 +1,24 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
+import StoryList from '../../components/StoryList';
 import logo from '../../assets/doist-logo.svg';
 
 function App() {
+  const [stories, updateStories] = useState([]);
+
   return (
     <div className="App">
       <header className="App-header">
         Hacker News Reader
       </header>
 
-      <footer className='App-footer'><img src={logo} className="App-doist-logo" /></footer>
+      <main className='App-main'>
+        <StoryList stories={stories} />
+      </main>
+
+      <footer className='App-footer'>
+        <img src={logo} className="App-doist-logo" alt="Doist logo" />
+        </footer>
     </div>
   );
 }
