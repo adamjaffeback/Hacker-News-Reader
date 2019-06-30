60bf0ce0fa66652fd6543bab3e04b6dbaf19bf8f
Adam Jaffe Back <adb33@email.arizona.edu>
2019-06-30 10:52:59 -0700

    (feat) If you can't get the nextStory, try again.

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -27,16 +27,26 @@ function App() {
     getFiveHundredNewStoryIds();
   }, []);
 
-  async function getNextStory () {
-    const nextItemId = fetchingCache.current.shift();
+  async function getNextStory (retryDelay = 1000) {
+    const nextItemId = fetchingCache.current[0];
     const url = `https://hacker-news.firebaseio.com/v0/item/${nextItemId}.json`;
-    const response = await fetch(url);
-    const story = await response.json();
 
-    updateStories(strs => strs.concat(story));
-
-    if (fetchingCache.current.length) {
-      getNextStory();
+    try {
+      const response = await fetch(url);
+      const story = await response.json();
+      updateStories(strs => strs.concat(story));
+      // story was fetched, get rid of it
+      fetchingCache.current.shift();
+      if (fetchingCache.current.length) {
+        getNextStory();
+      }
+    } catch (e) {
+      setTimeout(() => {
+        // if the delay has reached 10s, just keep polling at that rate
+        // otherwise, work your way up to 10s in 1s increments
+        let nextDelay = retryDelay > 10000 ? retryDelay : retryDelay + 1000;
+        getNextStory(nextDelay);
+      }, retryDelay);
     }
   }
 
