a9ef792099ce1c0f215c47fee64c3333e5fec1df
Adam Jaffe Back <adb33@email.arizona.edu>
2019-06-29 20:28:38 -0700

    (feat) Added infinite scroll with package to StoryItems.

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -1,24 +1,53 @@
 import React, {useState} from 'react';
 import './App.css';
-import StoryList from '../../components/StoryList';
+import StoryItem from '../../components/StoryItem';
+import InfiniteScroll from 'react-infinite-scroll-component';
 import logo from '../../assets/doist-logo.svg';
 
 function App() {
-  const [stories, updateStories] = useState([]);
+  const initialState = [];
+
+  for (let i = 0; i < 30; i++) {
+    initialState.push(
+      {id: i.toString(), url: 'www.google.com', title: `Test ${i}`, by: 'Adam', time: new Date().toString()}
+    );
+  }
+  const [stories, updateStories] = useState(initialState);
+
+  function fetchMoreStories () {
+    // a fake async api call like which sends
+    // 20 more records in 1.5 secs
+    const moreStories = [];
+    const currentStoryCount = stories.length;
+    for (let i = currentStoryCount; i < currentStoryCount + 20; i++) {
+      moreStories.push({id: i.toString(), url: 'www.google.com', title: `Test ${i}`, by: 'Adam', time: new Date().toString()});
+    }
+
+    setTimeout(() => {
+      updateStories([...stories, ...moreStories]);
+    }, 1500);
+  };
 
   return (
     <div className="App">
       <header className="App-header">
-        Hacker News Reader
+       <h3 style={{alignSelf: 'center'}}>Hacker News Reader</h3>
       </header>
 
       <main className='App-main'>
-        <StoryList stories={stories} />
+        <InfiniteScroll
+          dataLength={stories.length}
+          next={fetchMoreStories}
+          hasMore={true}>
+          {stories.map((story) => (
+            <StoryItem key={story.id} story={story} />
+          ))}
+        </InfiniteScroll>
       </main>
 
       <footer className='App-footer'>
         <img src={logo} className="App-doist-logo" alt="Doist logo" />
-        </footer>
+      </footer>
     </div>
   );
 }
